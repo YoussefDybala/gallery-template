f9a7261cc61e720b734f2263386453ae4de2bc69
Youssef Dybala <newyossef55o@gmail.com>
2024-05-13 02:39:12 +0300

    new commit

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+// let otherLinks = document.querySelector(".other-links")
+// let mainHeader = document.querySelector(".main-header")
+// let fetures = document.querySelector(".fet")
+// otherLinks.addEventListener("click" , function (){
+//     mainHeader.classList.add("open")
+//     fetures.addEventListener("click" ,function () {
+//         mainHeader.classList.remove("open")
+//     })
+// })
